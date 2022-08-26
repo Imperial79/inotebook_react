5195419e47a69ae88e5523edd9651572de835394
Avishek Verma <avishekverma79@gmail.com>
2022-08-26 18:05:30 +0530

    implemented password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator"); //used for validation of data
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "thisisasecretkey";
 const router = express.Router();
 
-// create a user using : POST "/api/auth"
+// create a user using : POST "/api/auth/createUser": No Login Required
 
 router.post(
-  "/",
+  "/createUser",
   [
     body("name", "Enter a valid name(length >= 3)").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
@@ -15,25 +17,43 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If invalid input/error, return bad request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      //check whether the user with this email already exist
+      let user = await User.findOne({ email: req.body.email });
+      console.log(user);
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "sorry a user with this email already exists" });
+      }
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a unique value for email",
-          message: err.message,
-        });
+      //hashing the password with salt using bcryptJS to secure it
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      //create a new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
       });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authToken });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Some error occurred");
+    }
   }
 );
 
